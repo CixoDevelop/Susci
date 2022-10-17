df0693d7d0b1d6313471f3c811fe7c4771ea6531
Cixo Develop <cixolaptop@gmail.com>
2022-10-17 23:50:00 +0200

    Recreate build system and add separate .c and .h

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GCC=avr-gcc #Or gcc for Your MCU
-SIZE=avr-size #Or Your size command
-MCU=attiny261 #Or Your MCU
-
-$GCC -o core.bin core.c -mmcu=$MCU -Os -Wall -Wextra -Wpedantic -Wno-unused-function -Wno-array-bounds
-$SIZE -C --mcu $MCU core.bin
